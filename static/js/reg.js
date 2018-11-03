b27b82f764dca6f0ab8e53ac42c65d67edd0d1e1
ARAVIND <avdar888@gmail.com>
2018-11-03 15:55:09 +0530

    html

---
+++ static/js/reg.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+	$(".sh").hide();
+	$("#shown").click(function(){
+        $(".sh").show();
+        
+
+    });
+    $("#hidden").click(function(){
+        $(".sh").hide();
+        
+    });
+});
