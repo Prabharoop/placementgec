8a920e70366013f06c59396927b8b747de92f87f
Prabharoop <ccprabharoop@gmail.com>
2018-11-05 03:07:41 +0530

    Updates

--- gecplacementcell/settings.py
+++ gecplacementcell/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'gecplacementcell.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'placementgec',
-        'USER': 'postgres',
-        'PASSWORD': 'JACKhoney@1234',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
