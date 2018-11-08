d1f0222534f6236e60c55b1c2688cc28b1c38ebc
prabharoop447 <ccprabharoop@gmail.com>
2018-11-08 08:08:29 +0530

    Models

--- mainapp/models.py
+++ mainapp/models.py
@@ -35,6 +35,41 @@ from django.db import models
 #     ('noto', 'Not Opted'),
 # )
 #
-
-
-
+# class Placement(models.Model):
+#     name = models.CharField(max_length = 50)
+#     reg = models.CharField(max_length = 50)
+#     email = models.EmailField(max_length=200)
+#     gender = models.CharField(max_length=20, choices = genders)
+#     course = models.CharField(max_length=20, choices = courses)
+#     number = models.CharField(max_length=20)
+#     branch = models.CharField(max_length=20, choices = branches)
+#     grad = models.DecimalField(max_digits=8)
+#     org = models.CharField(max_length=250)
+#     desig = models.CharField(max_length=250)
+#     domain = models.CharField(max_length=50,choices = domains)
+#     degree = models.CharField(max_length = 100)
+#     custatus = models.CharField(max_length=50, choices = status)
+#     univname = models.CharField(max_length=100)
+#     texta = models.TextField()
+#     textb = models.TextField()
+#     textc = models.TextField()
+#     textd = models.TextField()
+#     texte = models.TextField()
+#     textf = models.TextField()
+#     textg = models.TextField()
+#
+#     def __str__(self):
+#         return 'Name: %s Number: %s EMail: %s Course: %s Branch: %s Organisation: %s Designation: %s Graduation: %s' % ( self.name,
+#                                                                                                                     self.number,
+#                                                                                                                     self.email,
+#                                                                                                                     self.course,
+#                                                                                                                     self.branch,
+#                                                                                                                     self.org,
+#                                                                                                                     self.desig,
+#                                                                                                                     self.grad,
+#                                                                                                                 )
+#
+#
+#
+#
+#
