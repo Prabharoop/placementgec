576da3ef68222badd61553ad0edbca335cc4bafd
prabharoop447 <ccprabharoop@gmail.com>
2018-11-17 18:23:50 +0530

    herokunew

--- gecplacementcell/settings.py
+++ gecplacementcell/settings.py
@@ -134,7 +134,8 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/gecplacementcell/static/',
+    # '/gecplacementcell/static/'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
