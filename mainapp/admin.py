22cc6f2e23bd4a7448b9933e6578c24980b90c1e
prabharoop447 <ccprabharoop@gmail.com>
2018-11-26 17:27:27 +0530

    adminfilter

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -3,7 +3,7 @@ from .models import Placement
 # Register your models here.
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name', 'email','course','reg','gender','number','branch','grad', 'desig','org', 'domain', 'degree', 'custatus', 'univname','texta','choicea','textb','textc','choiceb','choicec','textd','texte','textf','textg')
-    list_filter = ('name','branch')
+    list_filter = ('branch','desig','domain','choicea','choiceb','choicec')
     search_fields = ('name','branch')
 
 admin.site.register(Placement,AuthorAdmin)
