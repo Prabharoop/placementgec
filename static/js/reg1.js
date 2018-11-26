3a099d4d4979b3f795bcdf6cdf2e17abce350386
ARAVIND <avdar888@gmail.com>
2018-11-26 19:40:41 +0530

    js

--- static/js/reg1.js
+++ static/js/reg1.js
@@ -1,17 +1,1 @@
 
-$(document).ready(function(){
-	$("#id_texte").hide();
-	$("#id_textf").hide();
-    $("#id_textg").hide();
-    $("#id_choicee_0").click(function(){
-        $("#id_texte").show();
-        $("#id_textf").hide();
-        $("#id_textg").hide();
-
-    });
-    $("#id_choicee_1").click(function(){
-        $("#id_texte").hide();
-        $("#id_textf").show();
-        $("#id_textg").show();
-    });
-});
