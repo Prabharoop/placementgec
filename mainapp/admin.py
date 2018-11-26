d29047f44d95b95cee0ec9872554bb0ec3b9402f
prabharoop447 <ccprabharoop@gmail.com>
2018-11-26 17:20:55 +0530

    admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Placement
 # Register your models here.
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email','course','number','branch','grad', 'desig','org')
+    list_display = ('name', 'email','course','reg','gender','number','branch','grad', 'desig','org', 'domain', 'degree', 'custatus', 'univname','texta','choicea','textb','textc','choiceb','choicec','textd','texte','textf','textg')
     list_filter = ('name','branch')
     search_fields = ('name','branch')
 
