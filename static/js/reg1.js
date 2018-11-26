766ab5e5fd8e790bc546dfbb65c0143f0616b4ca
ARAVIND <avdar888@gmail.com>
2018-11-26 23:13:35 +0530

    cssandjs

--- static/js/reg1.js
+++ static/js/reg1.js
@@ -1,1 +1,21 @@
+function checkVisibility(){
+    var formgroups=document.querySelectorAll(".form-group");
+    Array.prototype.forEach.call(formgroups,function(el,i){
+        var checkbox = el.querySelectorAll(".checkbox");
+        if(checkbox.length >0){
+            var checkboxElem = el.querySelectorAll("input")[0];
+            if(checkboxElem.id === "id_choicee_0"){
 
+            }
+            else if(checkboxElem.checked){
+                formgroups[i+1].style.display ="block";
+            }
+            else{
+                formgroups[i+1].style.display = "none";
+            }
+        }
+    });
+}
+for(var i=0; i<radios.length;i++){
+    radios[i].addEventListner("change",checkVisibility);
+}
