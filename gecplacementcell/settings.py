e56430f88906144fd3c699db8409a664fc533332
prabharoop447 <ccprabharoop@gmail.com>
2019-02-19 10:28:12 +0530

    sett

--- gecplacementcell/settings.py
+++ gecplacementcell/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gecplacementcell.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'gecplacementcell.herokuapp.com', 'localhost','www.gecskpalumni.in']
 
 
 # Application definition
